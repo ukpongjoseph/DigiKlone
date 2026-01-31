94105d02d1be9c87d21bbf197153c4022b9545d9
ukpongjoseph <jukpong499@gmail.com>
2026-01-31 21:23:58 +0100

    Added recent transaction list to digi save container

--- lib/pages/digi_save_balance.dart
+++ lib/pages/digi_save_balance.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:second_flutter/a_List_providers/theme_provider.dart';
 import 'package:second_flutter/components/digi_save_container.dart';
+import 'package:second_flutter/components/digi_save_recent_transactions.dart';
 
 class DigiSaveBalance extends StatefulWidget {
   const DigiSaveBalance({super.key});
@@ -16,18 +17,22 @@ class _DigiSaveBalanceState extends State<DigiSaveBalance> {
     ThemeData theme = context.read<ThemeProvider>().getTheme();
     double screenWidth = MediaQuery.of(context).size.width;
     bool isMobile = screenWidth < 600;
-    double spacing = isMobile ? 5:8;
+    double spacing = isMobile ? 5 : 8;
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        color: theme.colorScheme.surfaceContainerHigh,
+      body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.all(spacing *2),
-          decoration: BoxDecoration(),
-          child: Column(
-            children: [
-              DigiSaveContainer()
-            ],
+          color: theme.colorScheme.surfaceContainerHigh,
+          child: Container(
+            padding: EdgeInsets.all(spacing * 2),
+            decoration: BoxDecoration(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DigiSaveContainer(), 
+                DigiSaveRecentTransactions()
+              ],
+            ),
           ),
         ),
       ),
