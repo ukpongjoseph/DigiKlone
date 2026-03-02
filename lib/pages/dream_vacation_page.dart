9795b8311760ae90affdc0e09027754728defcab
ukpong joseph <aniekanjoseph499@gmail.com>
2026-03-02 14:56:07 +0100

    Started Dream Vacation Page

--- lib/pages/dream_vacation_page.dart
+++ lib/pages/dream_vacation_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:second_flutter/a_List_providers/theme_provider.dart';
+import 'package:second_flutter/theme/app_theme.dart';
 
 class DreamVacationPage extends StatefulWidget {
   const DreamVacationPage({super.key});
@@ -10,10 +13,25 @@ class DreamVacationPage extends StatefulWidget {
 class _DreamVacationPageState extends State<DreamVacationPage> {
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = context.read<ThemeProvider>().getTheme();
+    double screenWidth = MediaQuery.of(context).size.width;
+    bool isMobile = screenWidth<600;
+    double spacing = isMobile?5:8;
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text("Dream vacation Page"),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Dream Vacation",style: TextStyle(fontSize: isMobile?17:19,fontWeight: FontWeight.bold),),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.settings_outlined, color: theme == lightTheme? Colors.black:Colors.white,))
+        ],
+      ),
+      body: Container(
+        decoration : BoxDecoration(),
+        child: Column(
+          children: [
+            
+          ],
+        ),
       ),
     );
   }
